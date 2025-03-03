de256a659d1e8d5f8c8a4001fdf54a76d5c94fe9
PiyushMahore <156774805+PiyushMahore@users.noreply.github.com>
2025-03-03 23:43:22 +0530

    add Razorpay integration for order processing and payment handling

--- Backend/src/controllers/order.controller.js
+++ Backend/src/controllers/order.controller.js
@@ -4,46 +4,20 @@ import asyncHandler from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { Order } from "../models/order.model.js";
 import { sendOrderConfirmation } from "../utils/orderGmail.js";
+import Razorpay from "razorpay";
+var instance = new Razorpay({ key_id: `${process.env.ROZARPAY_APIKEY}`, key_secret: `${process.env.ROZARPAY_API_SECRET}` })
 
 const placeOrder = asyncHandler(async (req, res) => {
     const { address, city, zipCode, product, quantity, price } = req.body;
 
-    if ([address, city, zipCode, product, quantity, price].some((field) => field?.trim() == "")) {
-        throw new apiError(400, "Incomplete details failed to place order");
-    }
-
-    const user = await User.findById(req.user);
-
-    if (!user) {
-        throw new apiError(400, "You are not logged in please login first.");
-    }
-
-    const orderDetails = {
-        email: "piyushmahore48@gmail.com",
-        customerName: `${user.firstName} ${user.lastName}`,
-        productName: product,
-        quantity: quantity,
-        price: price,
-        address: address,
-        city: city,
-        zipcode: zipCode,
-    }
-
-    const mail = await sendOrderConfirmation(orderDetails);
-
-    if (!mail.messageId) {
-        throw new apiError(500, "Failed to place order please try again later.");
-    }
-
-    const order = await Order.create({ product, address, city, quantity, orderdBy: req.user, zipCode, totalAmount: price * quantity });
-
-    if (!order) {
-        throw new apiError(500, "Failed to place order. Please try again");
-    }
+    const order = await instance.orders.create({
+        amount: price || "50000",
+        currency: "INR",
+    });
 
     return res
         .status(200)
-        .json(new apiResponse(200, order, "Order added successfully"));
+        .json(new apiResponse(200, order, "Order generated successfully."));
 });
 
 export { placeOrder };
